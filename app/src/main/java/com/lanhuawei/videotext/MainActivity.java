29a3983f5e8343d08f7da45ad36e14d98655116a
Lanhuawei <974801031@qq.com>
2018-01-23 15:33:19 +0800

    Initial commit

---
+++ app/src/main/java/com/lanhuawei/videotext/MainActivity.java
@@ -0,0 +1,46 @@
+package com.lanhuawei.videotext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.xiao.nicevideoplayer.NiceVideoPlayer;
+import com.xiao.nicevideoplayer.NiceVideoPlayerManager;
+import com.xiao.nicevideoplayer.TxVideoPlayerController;
+
+public class MainActivity extends Activity {
+
+    private NiceVideoPlayer mNiceVideoPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNiceVideoPlayer = (NiceVideoPlayer) findViewById(R.id.nice);
+        mNiceVideoPlayer.setPlayerType(NiceVideoPlayer.TYPE_IJK); // IjkPlayer or MediaPlayer
+        mNiceVideoPlayer.setUp("http://img5.dfhon.cn/imagefiles/201801/20/2018012015024620388006.mp4", null);
+        TxVideoPlayerController controller = new TxVideoPlayerController(this);
+        controller.setTitle("办公室小野开番外了，居然在办公室开澡堂！老板还点赞？");
+        controller.setLenght(0);
+        Glide.with(this)
+                .load("http://tanzi27niu.cdsb.mobi/wps/wp-content/uploads/2017/05/2017-05-17_17-30-43.jpg")
+                .placeholder(R.drawable.img_default)
+                .crossFade()
+                .into(controller.imageView());
+        mNiceVideoPlayer.setController(controller);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        NiceVideoPlayerManager.instance().releaseNiceVideoPlayer();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (NiceVideoPlayerManager.instance().onBackPressd()) return;
+        super.onBackPressed();
+    }
+}
